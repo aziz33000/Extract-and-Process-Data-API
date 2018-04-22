7e147d3b182382ffdb55b9bfe4d108a2e342759e
aziz33000 <aziz.thabet@rocketmail.com>
2018-04-22 03:08:27 +0100

    second commit

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+import mysql.connector
+import sys
+
+def create_connection():
+	cnx = mysql.connector.connect(user='root', host='127.0.0.1', database='employees')
+	cursor = cnx.cursor()
+	query = ("SELECT * FROM employees ")
+	cursor.execute(query)
+	for (Age) in cursor:
+  		print("{} was hired on".format(Age))
+
+	cursor.close()
+	cnx.close()
+
 
 app = Flask(__name__)
- #continuous debugging 
 
 #View Part
 @app.route('/')
 def index():
+	
 	return render_template('home.html')
 
 @app.route('/about')
@@ -20,4 +34,4 @@ def api():
 
 
 if __name__ == '__main__':
-	app.run(debug = True) #continuous debugging d
+	app.run(debug = True) #continuous debugging 
